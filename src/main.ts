fdc12df1caef67a89ddbf52819fd17acfb84a14d
Pamela Sessa <karinasessa@gmail.com>
2026-09-17 19:48:33 -0300

    feat: exception filter global para errores de Prisma

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
+import { PrismaExceptionFilter } from './prisma/prisma-exception.filter.js';
 
 async function boostrap(){
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,9 @@ async function boostrap(){
     }),
   );
 
+  app.useGlobalFilters(new PrismaExceptionFilter());
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 boostrap();
